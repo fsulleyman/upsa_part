9cda1b39fb112e3d29238c7a4e7f09bd69ba870f
Fuseini Sulleyman <sulleymanfuseini1@gmail.com>
2025-11-07 10:56:03 +0000

    Updated db.php

--- db.php
+++ db.php
@@ -1,15 +1,21 @@
 <?php
-// PostgreSQL connection parameters
-$host = "localhost";
-$port = "5432";
-$dbname = "upsa_party";
-$user = "postgres";
-$password = "12345";
+$databaseUrl = getenv("DATABASE_URL");
 
-// Connect to PostgreSQL
+if (!$databaseUrl) {
+    die("Error: DATABASE_URL environment variable is not set.");
+}
+
+$parts = parse_url($databaseUrl);
+
+$host     = $parts['host'];
+$port     = $parts['port'] ?? 5432;
+$dbname   = ltrim($parts['path'], '/');
+$user     = $parts['user'];
+$password = $parts['pass'];
+
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
-    die("Error: Unable to connect to the database.");
+    die("Error: Unable to connect to the database. " . pg_last_error());
 }
 ?>
